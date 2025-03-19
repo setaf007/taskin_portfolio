93644f0d716d823cb8b4a60ee6fd2d2f2452dd52
Taskin <setaf007@hotmail.com>
2025-03-19 17:57:25 +0800

    edit

--- src/components/experienceSection/SingleExperience.jsx
+++ src/components/experienceSection/SingleExperience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SingleExperience = ({ experience }) => {
   return (
-    <div className='md:h-[350px] md:w-[240px] sm:h-auto sm:w-full 
+    <div className='md:h-auto md:w-[20%] sm:h-auto sm:w-full 
     border-2 border-orange border-dashed rounded-2xl mt-12 p-4'>
         <p className='font-bold text-cyan'>{experience.job}</p>
         <p className='text-orange'>{experience.company}</p>

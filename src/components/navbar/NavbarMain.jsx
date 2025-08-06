47b166e30ec52b87ab95bf6e0b62e88d765275a6
Taskin <setaf007@hotmail.com>
2025-08-06 14:46:35 +0800

    navbar change

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -11,20 +11,20 @@ const NavbarMain = () => {
     setMenuOpen(!menuOpen);
   };
   return (
-    <nav className='max-w-[1300px] mx-auto px-6 w-full fixed
-    left-[50%] transform -translate-x-[50%] z-20 flex gap-4 mt-2'>
+    <nav className='max-w-[1300px] mx-auto px-3 w-full fixed
+    left-[50%] transform -translate-x-[50%] z-20 flex gap-2 mt-2'>
       <div className='flex justify-between w-full max-w-[1200px] 
-      mx-auto bg-white items-center p-6 rounded-r-full 
-      rounded-l-full border-[0.5px] border-darkGrey'>
+      mx-auto bg-white items-center p-3 sm:p-4 lg:p-6 rounded-r-full 
+      rounded-l-full border-[0.5px] border-darkGrey transition-all duration-200'>
         <NavbarLogo/>
-          <div className={`${menuOpen ? 'sm:block' : 'sm:hidden'} lg:block gap-8`}>
+          <div className={`${menuOpen ? 'sm:block' : 'sm:hidden'} lg:block gap-4 sm:gap-6 lg:gap-8`}>
             <NavbarLinks/>
           </div>
         <NavbarBtn/>
       </div>
-      <div className='flex lg:hidden sm:block p-6 bg-white items-center
-      justify-center rounded-full border-[0.5px] border-darkGrey'>
-        <button className='text-xxl p-3 border border-darkGrey 
+      <div className='flex lg:hidden sm:block p-3 sm:p-4 bg-white items-center
+      justify-center rounded-full border-[0.5px] border-darkGrey transition-all duration-200'>
+        <button className='text-xl sm:text-2xl p-2 sm:p-3 border border-darkGrey 
         rounded-full text-black' onClick={toggleMenu}>
           <GiHamburgerMenu />
         </button>
